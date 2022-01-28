42b06f9cd9a8fda126665b5fec982962a0593969
lucwas <lcsilvap@gmail.com>
2022-01-27 22:30:06 -0300

    After my own changes for CircleCI Integration

---
+++ section-06-model-serving-api/fastapi-quick-demo/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/square")
+async def square(num: int):
+    result = num**2
+    return {"squared": result }
